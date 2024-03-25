a1fd89dae903de6205274bb80d71a701296c4985
aditya <jaditya98@gmail.com>
2024-03-25 23:52:39 +0530

    add search api logic

---
+++ backend/src/routes/hotels.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/search", async (req: Request, res: Response) => {
+  try {
+    const pageSize = 5;
+    const pageNumber = parseInt(
+      req.query.page ? req.query.page.toString() : "1"
+    );
+  } catch (error) {}
+});
