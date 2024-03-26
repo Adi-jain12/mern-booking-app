1c943b0a8a435dea74ede4e2f070c0af32a39ccb
aditya <jaditya98@gmail.com>
2024-03-26 23:55:07 +0530

    add fetch hotel api

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -171,3 +171,13 @@ export const searchHotels = async (
 
   return response.json();
 };
+
+export const fetchHotelById = async (hotelId: string) => {
+  const response = await fetch(`${API_BASE_URL}/api/hotels/${hotelId}`);
+
+  if (!response.ok) {
+    throw new Error("Error fetchig Hotels");
+  }
+
+  return response.json();
+};
